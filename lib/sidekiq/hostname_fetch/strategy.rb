88f9defcedaab4d32c60ffc62a6ecaa42d13141e
Sergey Prikhodko <prikha@gmail.com>
2015-11-23 15:05:19 +0300

    Honor redis cmd restrictions

--- lib/sidekiq/hostname_fetch/strategy.rb
+++ lib/sidekiq/hostname_fetch/strategy.rb
@@ -5,8 +5,13 @@ module Sidekiq
   module HostnameFetch
     class Strategy < ::Sidekiq::BasicFetch
       def initialize(options)
-        super
-        @queues = @queues.map { |q| [q, "#{q}_host_#{current_hostname}"] }.flatten
+        @strictly_ordered_queues = !!options[:strict]
+        @queues = options[:queues].map { |q| "queue:#{q}" }
+        @queues.unshift *@queues.map { |q| "#{q}_host_#{current_hostname}" }
+        if @strictly_ordered_queues
+          @queues = @queues.uniq
+          @queues << TIMEOUT
+        end
       end
 
       private
--- spec/sidekiq/strategy_spec.rb
+++ spec/sidekiq/strategy_spec.rb
@@ -1,8 +1,8 @@
 require 'spec_helper'
 
 describe Sidekiq::HostnameFetch::Strategy do
-  let(:options) {{ queues: queues }}
   let(:queues) { %w(queue1 queue1 queue2 queue2) }
+  let(:options) {{ queues: queues }}
 
   it do
     allow_any_instance_of(described_class).to receive(:current_hostname).and_return("job-01.rspec-runner.com")
@@ -10,9 +10,8 @@ describe Sidekiq::HostnameFetch::Strategy do
 
     fetcher_queues = fetcher.instance_variable_get("@queues")
 
-    queues_with_hostname = queues.map do |q|
-      ["queue:#{q}", "queue:#{q}_host_job-01.rspec-runner.com"]
-    end.flatten
-    expect(fetcher_queues).to eq(queues_with_hostname)
+    queues_with_hostname = queues.map { |q| "queue:#{q}_host_job-01.rspec-runner.com" }
+    queues_without_hostname = queues.map { |q| "queue:#{q}" }
+    expect(fetcher_queues).to eq(queues_with_hostname + queues_without_hostname)
   end
 end
